d0e7771bc4c3319eb277a992f7ae51c2d299fb97
YechengChu <szcyc27@163.com>
2020-07-20 16:19:42 +0800

    modified interface test

--- interfaceTest.go
+++ interfaceTest.go
@@ -15,9 +15,18 @@ func (sq *Square) Area() float32 {
 	return sq.side * sq.side
 } // Area
 
+type Rectangle struct {
+	length, width float32
+}
+
+// Rectangle has Area() function so can be a Shaper
+func (r Rectangle) Area() float32 {
+	return r.length * r.width
+} // Area
+
 func main() {
 	sq1 := new(Square)
-	sq1.side = 3
+	sq1.side = 3.5
 
 	var areaIntf Shaper
 	areaIntf = sq1
@@ -25,5 +34,26 @@ func main() {
 	// areaIntf := Shaper(sq1)
 	// or even:
 	// areaIntf := sq1
-	fmt.Printf("The square has area: %f\n", areaIntf.Area())
+	fmt.Println("The square1 has area: ", areaIntf.Area())
+
+	fmt.Println("---------------------------------------")
+
+	r := Rectangle{5, 3} // Area() of Rectangle needs a value
+	sq2 := &Square{5}    // Area() of Square needs a pointer
+	// The above two lines the same effect as the following 6 lines
+	// rAddr := new(Rectangle)
+	// rAddr.length = 5
+	// rAddr.width = 3
+	// r := *rAddr
+	// sq2 := new(Square)
+	// sq2.side = 5
+
+	// shapes := []Shaper{Shaper(r), Shaper(sq2)}
+	// or shorter
+	shapes := []Shaper{r, sq2}
+	fmt.Println("Looping through shapes for area ...")
+	for n, _ := range shapes {
+		fmt.Println("Shape details: ", shapes[n])
+		fmt.Println("Area of this shape is: ", shapes[n].Area())
+	} // for
 } // main
