38823c1a76f07a2d9c90d6af4022b9cdd4c91312
YechengChu <szcyc27@163.com>
2020-07-22 09:52:52 +0800

    modified interfaceType to explore more situations

---
+++ interfaceType.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Square struct {
+	side float32
+}
+
+type Circle struct {
+	radius float32
+}
+
+type Shaper interface {
+	Area() float32
+}
+
+type MoreShaper interface {
+	Shaper
+	Perimeter() float32
+}
+
+func main() {
+	var areaIntf Shaper = &Square{5}
+	var aSquare = Square{4}
+	var areaIntf2 Shaper = &Circle{2}
+	fmt.Println("--------断言类型是一个具体类型--------")
+	// Is Square the type of areaIntf ?
+	if t, ok := areaIntf.(*Square); ok {
+		fmt.Printf("The type of areaIntf is: %T\n", t)
+		fmt.Printf("t is: %v\n", t)
+	} // if
+	if u, ok := areaIntf.(*Circle); ok {
+		fmt.Printf("The type of areaIntf is: %T\n", u)
+	} else {
+		fmt.Println("areaIntf does not contain a variable of type Circle")
+		fmt.Printf("u is: %v\n", u)
+	} // if ... else
+	// Is Square the type of &aSquare ?
+	if t, ok := (Shaper)(&aSquare).(*Square); ok {
+		fmt.Printf("The type of &aSquare is: %T\n", t)
+		fmt.Printf("t is: %v\n", t)
+	} // if
+	fmt.Println("--------断言类型是一个接口--------")
+	// is areaIntf a Shaper ?
+	if z, ok := areaIntf.(Shaper); ok {
+		fmt.Println("The of areaIntf is a Shaper")
+		fmt.Printf("z is: %v\n", z)
+	} // if
+	// is areaIntf2 a MoreShaper ?
+	if u, ok := areaIntf2.(MoreShaper); ok {
+		fmt.Printf("The type of areaIntf2 is: %T\n", u)
+	} else {
+		fmt.Println("areaIntf2 is not an interface MoreShaper")
+		fmt.Printf("u is: %v\n", u)
+	} // if ... else
+	// is &aSquare a MoreShaper ?
+	if z, ok := (Shaper)(&aSquare).(MoreShaper); ok {
+		fmt.Println("The &aSquare is a MoreShaper")
+		fmt.Printf("z is: %v\n", z)
+	} // if
+	fmt.Println("--------switch--------")
+	// testing with switch:
+	switch t := areaIntf.(type) {
+	case *Square:
+		fmt.Printf("Type Square %T with value %v\n", t, t)
+	case *Circle:
+		fmt.Printf("Type Circle %T with value %v\n", t, t)
+	default:
+		fmt.Printf("Unexpected type %T", t)
+	} // switch
+} // main
+
+func (sq *Square) Area() float32 {
+	return sq.side * sq.side
+} // Area
+
+func (sq *Square) Perimeter() float32 {
+	return sq.side * 4
+} // Perimeter
+
+func (ci *Circle) Area() float32 {
+	return ci.radius * ci.radius * math.Pi
+} // Area
