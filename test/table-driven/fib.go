0f9d1c292bb00ef25eddf9781e51e56dcc67690e
YechengChu <szcyc27@163.com>
2020-08-03 16:52:26 +0800

    modified fib_test

--- test/table-driven/fib.go
+++ test/table-driven/fib.go
@@ -3,6 +3,6 @@ package main
 func Fib(n int) int {
 	if n < 2 {
 		return n
-	}
+	} // if
 	return Fib(n-1) + Fib(n-2)
-}
+} // Fib
--- test/table-driven/fib_test.go
+++ test/table-driven/fib_test.go
@@ -2,24 +2,39 @@ package main
 
 import "testing"
 
+// 对号（√）
+const checkMark = "\u2713"
+// 叉号（×）
+const ballotX = "\u2717"
+
 func TestFib(t *testing.T) {
 	var fibTests = []struct {
-		in       int // input
+		in       int // input value
 		expected int // expected result
 	}{
+		// pairs of input value and expected result
 		{1, 1},
 		{2, 1},
 		{3, 2},
 		{4, 3},
 		{5, 5},
 		{6, 8},
+		// add a failing test
+		// {7,10},
 		{7, 13},
 	}
 
+	// for elements in fibTests
 	for _, tt := range fibTests {
+		// call Fib function use in value
 		actual := Fib(tt.in)
 		if actual != tt.expected {
-			t.Errorf("Fib(%d) = %d; expected %d", tt.in, actual, tt.expected)
+			// if actual result not equal to expected result
+			// print error
+			t.Errorf("%v Fib(%d) = %d; expected %d", ballotX, tt.in, actual, tt.expected)
+		} else {
+			// else, print log
+			t.Logf("%v Fib(%d) = %d", checkMark, tt.in, actual)
 		}
-	}
-}
+	} // for
+} // TestFib
